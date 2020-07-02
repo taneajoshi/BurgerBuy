f2ba2cb454f28450188991b5c2db06e87422053f
taneajoshi <taneajoshi@gmail.com>
2020-07-02 11:41:59 +0530

    Creating Components

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/aux';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component{
+    render(){
+        return (
+            <Aux>
+             <Burger />
+            <div>
+                Build Controls
+            </div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;

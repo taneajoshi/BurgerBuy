a18e3a6ba68524f9851cbdf04677d494f3f67adb
taneajoshi <taneajoshi@gmail.com>
2020-07-04 18:17:20 +0530

    Fixed Menu issues

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,11 +4,12 @@ import Logo from '../../Logo/Logo';
 import Navigation from '../NavigationItems/NavigationItems';
 const toolbar = (props)=>(
    <header className={classes.Toolbar}>
-      <div className={classes.MenuLogo}><Logo/></div>
-      <div className={classes.MenuItem}>MENU</div>
-      <nav>
-         <Navigation/>
-      </nav>
+    <div className={classes.MenuLogo}><Logo/></div>
+    <nav>
+        <Navigation/>
+    </nav>
+    <div className={classes.MenuItem}>MENU</div>
+    
 
    </header>
 );

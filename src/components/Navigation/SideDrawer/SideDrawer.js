ddda80404b29dcae6fe53bb7e3a28c30ba0c12af
taneajoshi <taneajoshi@gmail.com>
2020-07-05 15:36:01 +0530

    Backdrop reuse

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,8 +2,12 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auc from '../../../hoc/aux';
 const sideDrawer = (props) =>{
     return(
+        <Aux>
+        <Modal/>
         <div className={classes.SideDrawer}>
             <div className={classes.Logo}> <Logo/></div>
          
@@ -11,6 +15,7 @@ const sideDrawer = (props) =>{
               <NavigationItems/>
           </nav>
         </div>
+        </Aux>
     );
     }
 export default sideDrawer;

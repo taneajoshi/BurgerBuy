14c667e10b243512f93d5503abc0169ce7f64e44
taneajoshi <taneajoshi@gmail.com>
2020-07-05 17:17:00 +0530

    Menu responsiveness

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MenuLogo from '../../assets/images/menu.png';
 import classes from './Menu.css';
-const Menu =(props)=>(
-    <div onClick={props.clicked} className={classes.Menu} >
+const Menu =(props)=>{
+    let classList =[classes.Menu, classes.MobileOnly];
+    return(
+    <div onClick={props.clicked} className={classList.join(' ')} >
     <img src={MenuLogo} alt={MenuLogo}/>
 </div>
-);
+)}
 
 export default Menu;

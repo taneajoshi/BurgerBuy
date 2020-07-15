58fd92fcf2dfe67d628316145398ade428fa8f14
taneajoshi <taneajoshi@gmail.com>
2020-07-16 00:43:35 +0530

    Compound will unmount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React  , {Component} from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from  './containers/BurgerBuilder/BurgerBuilder'
-function App() {
-  return (
+class App extends Component {
+ state={
+   show:true
+ }
+
+ componentDidMount(){
+   setTimeout(()=>{
+     this.setState({show:false});
+   },5000);
+ }
+  render(){
+    return (
     <div >
     <Layout>
-     <BurgerBuilder />
+     {this.state.show ? <BurgerBuilder /> : null }
     </Layout>
     </div>
-  );
+  );}
 }
 
 
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -18,7 +18,9 @@ const withErrorHandler =(WrappedComponent, axios )=>{
         }
 
         componentWillUnmount(){
-            
+            console.log('Will unmount', this.resInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorConfirmedHandler =() =>{
